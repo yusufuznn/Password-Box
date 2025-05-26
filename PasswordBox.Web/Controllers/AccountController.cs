cac95b3fdf9a04ee005df52d95f2ccd22f969e8b
Yusuf Uzun <Yusuf Uzun@DESKTOP-I9V17A6>
2025-05-26 22:40:59 +0300

    register commit

---
+++ PasswordBox.Web/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PasswordBox.Web.Models.ViewModels;
+
+namespace PasswordBox.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerViewModel.Username,
+                Email = registerViewModel.Email
+            };
+
+            var identityResult = await userManager.CreateAsync(identityUser, registerViewModel.Password);
+
+            if (identityResult.Succeeded)
+            {
+                var roleIdenityResult = await userManager.AddToRoleAsync(identityUser, "User");
+
+                if (roleIdenityResult.Succeeded)
+                {
+                    return RedirectToAction("Register");
+
+                }
+
+            }
+
+            return View();
+
+
+
+
+        }
+
+    }
+}
