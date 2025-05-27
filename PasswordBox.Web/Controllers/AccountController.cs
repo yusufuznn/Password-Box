8033097f7a958fdee5210f90c3f821efe955d95f
Yusuf Uzun <Yusuf Uzun@DESKTOP-I9V17A6>
2025-05-27 20:09:56 +0300

    user login and logout

--- PasswordBox.Web/Controllers/AccountController.cs
+++ PasswordBox.Web/Controllers/AccountController.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PasswordBox.Web.Models.ViewModels;
+using PasswordBox.Web.Models.ViewModels;
 
 namespace PasswordBox.Web.Controllers
 {
     public class AccountController : Controller
     {
         private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
 
-        public AccountController(UserManager<IdentityUser> userManager)
+        public AccountController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
         {
             this.userManager = userManager;
+            this.signInManager = signInManager;
         }
 
 
@@ -46,10 +50,47 @@ namespace PasswordBox.Web.Controllers
 
             return View();
 
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            var signInResult = await signInManager.PasswordSignInAsync(loginViewModel.Username,
+                loginViewModel.Password, false, false);
+
+            if (signInResult != null && signInResult.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
 
 
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
 
         }
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
